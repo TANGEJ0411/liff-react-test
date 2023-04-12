e666e8c37fda66ffb4df5104d1d4809354cf66c2
IsTree <IsTree@gmail.com>
2023-04-12 12:22:26 +0800

    first-try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import liff from '@line/liff';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loginReq, setLoginReq] = useState(false)
+  const [profile, setProfile] = useState({})
+  const [isLoggedIn, setIsloggedin] = useState(false)
+  useEffect(() => {
+    if (loginReq || liff.isInClient()) {
+      liff
+        .init({
+          liffId: "1660858533-loqWyNQE", // Use own liffId
+        })
+        .then(() => {
+          // Start to use liff's api
+          console.log("初始化成功");
+          if (!liff.isLoggedIn()) {
+            console.log("你還沒登入Line哦！");
+            liff.login();
+          } else {
+            console.log("你已經登入Line哦！");
+            setIsloggedin(true)
+            const idToken = liff.getDecodedIDToken()
+            setProfile(idToken)
+            console.log(idToken)
+            alert(`${idToken.name}, ${idToken.email}, ${liff.getProfile().userId}`)
+          }
+        })
+        .catch((err) => {
+          // Error happens during initialization
+          console.log("INIT", err.code, err.message);
+        });
+    }
+  }, [])
+
+  useEffect(() => { }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="wrap position-relative">
+      <button onClick={() => { setLoginReq(true) }}>{isLoggedIn ? '登出' : '登入'}</button>
+      {/* {profile.picture && (<img src={profile.picture} />)} */}
+      <div className='d-flex justify-content-between mx-3'>
+        <h3>我的票夾</h3>
+        <button>轉讓</button>
+      </div>
+      <div className='d-flex justify-content-center mx-3'>
+        <h6 className='border-tic flex-fill text-center'>預約券</h6>
+        <h6 className='border-tic flex-fill text-center'>即享券</h6>
+      </div>
+      <div className='tic-box'>
+        <div className='d-md-flex justify-content-md-center'>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+        </div>
+        <div className='d-md-flex justify-content-md-center'>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+        </div>
+        <div className='d-md-flex justify-content-md-center'>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+        </div>
+        <div className='d-md-flex justify-content-md-center'>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+        </div>
+        <div className='d-md-flex justify-content-md-center'>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+          <div className='tic my-1 mx-auto mx-md-2'>{`我是${profile.name}的票`}</div>
+        </div>
+      </div>
+      <div className='icon-box d-flex justify-content-around position-fixed'>
+        <p className='mt-2 mb-0'>商店</p>
+        <p className='mt-2 mb-0'>票券</p>
+        <p className='mt-2 mb-0'>行程</p>
+        <p className='mt-2 mb-0'>會員</p>
+      </div>
+      {/* <div className='icon-box d-flex justify-content-around position-fixed'>
+        <p className='mt-2 mb-0'>商店</p>
+        <p className='mt-2 mb-0'>點數</p>
+        <p className='mt-2 mb-0'>等級</p>
+        <p className='mt-2 mb-0'>會員</p>
+      </div> */}
+    </div >
   );
 }
 
---
+++ src/LoginButton.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react'
+// import { AgGridReact } from 'ag-grid-react';
+import axios from 'axios';
+// import 'ag-grid-community/styles//ag-grid.css';
+// import 'ag-grid-community/styles//ag-theme-alpine.css';
+
+function LoginButton() {
+    // const state = date()
+    // const [data, setData] = useState({
+    //     columnDefs: [
+    //         { headerName: "Make", field: "make" },
+    //         { headerName: "Model", field: "model" },
+    //         { headerName: "Price", field: "price" },
+    //         { headerName: "Popular", field: "popular" }
+
+    //     ],
+    //     rowData: [
+    //         { make: "Toyota", model: "Celica", price: 35000, popular: 1 },
+    //         { make: "Ford", model: "Mondeo", price: 32000, popular: 2 },
+    //         { make: "Porsche", model: "Boxster", price: 72000, popular: 3 },
+    //         { make: "Honda", model: "Fit", price: 33000, popular: 4 },
+    //     ]
+    // })
+    const [code, setCode] = useState("")
+    const [token, setToken] = useState("")
+    // const []
+    useEffect(() => {
+        const url = new URL(window.location.href) //當前網址
+        console.log(url)
+        console.log(url.searchParams.get('code'))
+        setCode(url.searchParams.get('code'))
+    }, [])
+
+    useEffect(() => {
+        async function getToken() {
+            if (code !== "") {
+                try {
+                    const response = await axios.post('https://api.line.me/oauth2/v2.1/token', {
+                        client_id: '1660858533',
+                        grant_type: 'authorization_code',
+                        code,
+                        client_secret: '4e16d5fc11ffda0a5e6189770cf40c7a',
+                        redirect_uri: 'http://localhost:3000'
+                    }, {
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }
+                    })
+                    setToken(response.data.access_token);
+                } catch (error) {
+                    // console.error(error);
+                }
+            }
+        }
+        getToken();
+    }, [code])
+    useEffect(() => {
+        async function getUser() {
+            if (token !== "") {
+                try {
+                    const response = await axios.get('https://api.line.me/v2/profile', {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        }
+                    })
+                    console.log(response)
+                    alert(`uid:${response.data.userId}\n name:${response.data.displayName}`)
+                } catch (error) {
+                    // console.error(error);
+                }
+            }
+        }
+        getUser();
+    }, [token])
+
+    return (
+        <>
+            {/* <div
+                className="ag-theme-alpine"
+                style={{
+                    height: '500px',
+                    width: '802px'
+                }}
+            >
+                <AgGridReact
+                    columnDefs={data.columnDefs}
+                    rowData={data.rowData}>
+                </AgGridReact>
+            </div> */}
+            {token && (<img src='https://profile.line-scdn.net/0h2nNLW6zCbUFjNXoJ2jITPhNlbitARDRTTAAnLwU0ZHIKDH8TH1UkJV49ZiZZDH4QSQNxclBhYSRvJhonfWORdWQFM3ZZBCMTRlcgpQ' />)}
+            <a href='https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1660858533&redirect_uri=http://localhost:3000&state=12345abcde&scope=profile%20openid'>LINE</a>
+        </>
+    )
+}
+
+export default LoginButton
